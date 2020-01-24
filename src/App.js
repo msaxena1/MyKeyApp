b52623160134b4e1a1b0eac25ce53cbbbe0f3d81
Manoj Saxena <msaxena1@gmail.com>
2020-01-23 21:30:53 -0500

    Moved isAuthenticated to functional Login component

--- src/App.js
+++ src/App.js
@@ -2,15 +2,11 @@ import React from 'react';
 import './App.css'
 import Login from './container/login/Login';
 
-const isAuthenticated = true;
-
-const login = isAuthenticated ? <Login /> : null;
-
 const App = () => { 
     return (
       <div>
           <h1>Welcome to MyKepApp</h1>
-          {login}
+          <Login />
       </div>
     );
 }
--- src/container/login/Login.js
+++ src/container/login/Login.js
@@ -2,15 +2,14 @@ import React from 'react';
 import TextInput from '../../components/TextInput/TextInput';
 import Button from '../../components/Button/Button';
 
+const isAuthenticated = true;
 
-const Login = () => {
-    return (
-        <div>
-            <TextInput label='secret1' />
-            <TextInput label='secret2'/>
-            <Button name='login' />
-        </div>
-    );
-};
+const Login = () => isAuthenticated ? (
+    <div>
+        <TextInput label='secret1' />
+        <TextInput label='secret2'/>
+        <Button name='login' />
+    </div>
+    ) : null ;
 
 export default Login;
