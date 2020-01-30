e6ce9c3b660ed2aed8f14fad6a0fbf51878286fd
Manoj Saxena <msaxena1@gmail.com>
2020-01-29 23:03:09 -0500

    added useEffect and useReducer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import React, { useState } from 'react';
+import React, { useContext, useEffect, useReducer } from 'react';
 import './App.css'
 import Login from './container/login/Login';
 import Tile from './components/Tile/Tile';
+import Button from './components/Button/Button';
+import { AuthenticationContext } from './store/Store';
 
 const content = {
   username: 'Mike',
@@ -9,21 +11,72 @@ const content = {
   email: 'mike@yahoo.com'
 }
 
-const tiles = [
-  { url: 'https://www.bankofamerica.com/', name: 'Bank Of America', idx: 0 },
-  { url: 'https://www.gmail.com/', name: 'Gmail', idx: 1 },
-  { url: 'https://www.facebook.com/', name: 'Facebook', idx: 2 },
-  { url: 'https://www.twitter.com/', name: 'Twitter', idx: 3 },
-  { url: 'https://www.github.com/', name: 'Github', idx: 4 }
-]
+
+
+const divAppName = {
+  width: '70%',
+  display: 'inline-block'
+};
+
+const divNewButton = {
+  width: '20%',
+  display: 'inline-block',
+};
+
+const reducer = ( state, action ) => {
+  switch ( action.type ) {
+    case 'INIT':
+      return action.payload;
+    case 'ADD':
+      return [ ...state, action.payload ];
+    case 'DELETE':
+      const keys = [ ...state ];
+      keys.pop();
+      return keys;
+    default:
+      return state;
+  }
+};
 
 const App = () => { 
+  const authContext = useContext( AuthenticationContext );
+  const isAuthenticated = authContext.isAuthenticated;
+  const addNewButton = isAuthenticated ? (
+    <div style={ divNewButton } >
+      <Button name='Add New' click={''} />
+    </div>
+    ) : 
+    null ;
+
+    const [ keyList, updateKeyList ] = useReducer( reducer, [] );
+
+    useEffect( () => {
+      Promise.resolve() // fetch()
+      .then( () => {
+        const keysFromFile = [
+          { url: 'https://www.bankofamerica.com/', name: 'Bank Of America', idx: 0 },
+          { url: 'https://www.gmail.com/', name: 'Gmail', idx: 1 },
+          { url: 'https://www.facebook.com/', name: 'Facebook', idx: 2 },
+          { url: 'https://www.twitter.com/', name: 'Twitter', idx: 3 },
+          { url: 'https://www.github.com/', name: 'Github', idx: 4 }
+        ];
+
+        updateKeyList( { type: 'INIT', payload: keysFromFile } );
+      } )
+      .catch( (err) => console.log(err) );
+    });
 
     return (
       <div>
-          <h1>Welcome to MyKepApp</h1>
+        <div>
+          <div style={ divAppName } >
+            <h1>MyKepApp</h1>
+          </div>
+          { addNewButton }
+        </div>
+          
           <Login />
-          { tiles.map( index => (
+          { keyList.map( index => (
             <Tile 
               url={index.url} 
               name={index.name} 
